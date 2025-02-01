b38543a085bf8c177f64bae33d2647e9a0bc3787
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-02 00:15:52 +0530

    Update provider.tf

--- Infra/Environments/dev/provider.tf
+++ Infra/Environments/dev/provider.tf
@@ -2,11 +2,39 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+data "aws_eks_cluster" "cluster" {
+  name = "my-eks-cluster"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = "my-eks-cluster"
+}
+
+
+data "aws_eks_cluster" "cluster" {
+  name = "my-eks-cluster"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = "my-eks-cluster"
+}
+
 provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
+
+
+
+/*provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   token                  = data.aws_eks_cluster_auth.eks.token
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
+} */ 
 
 provider "helm" {
   kubernetes {
