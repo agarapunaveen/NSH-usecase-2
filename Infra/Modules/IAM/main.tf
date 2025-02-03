550b53565e82423112f3597e2985f6b8449b6eb3
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-03 09:19:10 +0530

    Update main.tf

--- Infra/Modules/IAM/main.tf
+++ Infra/Modules/IAM/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+  name = "eks-cluster-roles"
   assume_role_policy = jsonencode({
     Statement = [{
       Effect = "Allow"
@@ -26,7 +26,7 @@ resource "aws_iam_role" "eks_node_role" {
 }
 
 resource "aws_iam_policy" "eks_k8s_access" {
-  name        = "eks-k8s-access"
+  name        = "eks-k8s-acces"
   description = "Policy to grant Kubernetes RBAC access to the EKS cluster"
 
   policy = jsonencode({
