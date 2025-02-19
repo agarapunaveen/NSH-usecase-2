47c38f34943d5de53932364b2157b2f17b885713
agarapunaveen <67690657+agarapunaveen@users.noreply.github.com>
2025-02-19 17:06:30 +0530

    Update main.tf

--- Infra/Modules/IAM/main.tf
+++ Infra/Modules/IAM/main.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role" "eks_node_role" {
 }
 
 resource "aws_iam_policy" "eks_k8s_access" {
-  name        = "eks-k8s_iam"
+  name        = "eks-k8s_iam-policy"
   description = "Policy to grant Kubernetes RBAC access to the EKS cluster"
 
   policy = jsonencode({
